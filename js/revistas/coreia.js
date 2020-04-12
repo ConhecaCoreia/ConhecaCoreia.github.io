0b9b8dd2b76c5547b9cfec68c8a6b93c20c8b062
Thalles Campagnani <thallescampagnani@gmail.com>
2020-04-12 19:17:16 -0300

    Adicionando os anos de 2020 a 2017 da revista KOREA

--- js/revistas/coreia.js
+++ js/revistas/coreia.js
@@ -1,5 +1,298 @@
 var coreia = [
     'Lista de edições do pictorial mensal: "República Popular Democrática da COREIA".',
+    '2020',
+    /*Descomente quando fazerem o uploud de mais revistas*/
+    /*{
+        "nome": "RPD da COREIA - Dezembro de 2020",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2020-12.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Novembro de 2020",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2020-11.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Outubro de 2020",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2020-10.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Setembro de 2020",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2020-9.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Agosto de 2020",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2020-8.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Julho de 2020",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2020-7.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Junho de 2020",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2020-6.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },*/
+    {
+        "nome": "RPD da COREIA - Maio de 2020",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2020-5.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Abril de 2020",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2020-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Março de 2020",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2020-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Fevereiro de 2020",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2020-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Janeiro de 2020",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2020-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    '2019',
+    {
+        "nome": "RPD da COREIA - Dezembro de 2019",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2019-12.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Novembro de 2019",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2019-11.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Outubro de 2019",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2019-10.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Setembro de 2019",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2019-9.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Agosto de 2019",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2019-8.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Julho de 2019",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2019-7.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Junho de 2019",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2019-6.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Maio de 2019",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2019-5.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Abril de 2019",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2019-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Março de 2019",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2019-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Fevereiro de 2019",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2019-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Janeiro de 2019",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2019-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    '2018',
+    {
+        "nome": "RPD da COREIA - Dezembro de 2018",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2018-12.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Novembro de 2018",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2018-11.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Outubro de 2018",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2018-10.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Setembro de 2018",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2018-9.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Agosto de 2018",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2018-8.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Julho de 2018",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2018-7.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Junho de 2018",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2018-6.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Maio de 2018",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2018-5.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Abril de 2018",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2018-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Março de 2018",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2018-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Fevereiro de 2018",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2018-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Janeiro de 2018",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2018-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    '2017',
+    {
+        "nome": "RPD da COREIA - Dezembro de 2017",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2017-12.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Novembro de 2017",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2017-11.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Outubro de 2017",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2017-10.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Setembro de 2017",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2017-9.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Agosto de 2017",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2017-8.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Julho de 2017",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2017-7.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Junho de 2017",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2017-6.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Maio de 2017",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2017-5.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Abril de 2017",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2017-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Março de 2017",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2017-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Fevereiro de 2017",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2017-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Janeiro de 2017",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2017-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
     '2016',
     {
         "nome": "RPD da COREIA - Dezembro de 2016",
@@ -44,13 +337,13 @@ var coreia = [
         "texto": ""
     },
     {
-        "nome": "RPD da COREIA - Abril de 2016",
+        "nome": "RPD da COREIA - Maio de 2016",
         "link": "https://knowkorea.github.io/PDF/KOREA-2016-5.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "RPD da COREIA - Maio de 2016",
+        "nome": "RPD da COREIA - Abril de 2016",
         "link": "https://knowkorea.github.io/PDF/KOREA-2016-4.pdf",
         "figura": "bandeira.png",
         "texto": ""
