a68f0e44d3141b0d9130b89cf359a5df6f87a7d6
Thalles Campagnani <thallescampagnani@gmail.com>
2020-04-12 18:57:46 -0300

    Mudando o nome para The Pyongyang Times

--- js/revistas/times.js
+++ js/revistas/times.js
@@ -1,67 +1,67 @@
 var times = [
-    'Lista de edições do jornal semanal: "PYONGYANG TIMES".',
+    'Lista de edições do jornal semanal: "THE PYONGYANG TIMES".',
     {
-        "nome": "Pyongyang Times - 13° Semana de 2020",
+        "nome": "The Pyongyang Times - 13° Semana de 2020",
         "link": "https://knowkorea.github.io/PDF/TIMES-2020-13.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Pyongyang Times - 12° Semana de 2020",
+        "nome": "The Pyongyang Times - 12° Semana de 2020",
         "link": "https://knowkorea.github.io/PDF/TIMES-2020-12.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Pyongyang Times - 11° Semana de 2020",
+        "nome": "The Pyongyang Times - 11° Semana de 2020",
         "link": "https://knowkorea.github.io/PDF/TIMES-2020-11.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Pyongyang Times - 10° Semana de 2020",
+        "nome": "The Pyongyang Times - 10° Semana de 2020",
         "link": "https://knowkorea.github.io/PDF/TIMES-2020-10.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Pyongyang Times - 9° Semana de 2020",
+        "nome": "The Pyongyang Times - 9° Semana de 2020",
         "link": "https://knowkorea.github.io/PDF/TIMES-2020-9.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Pyongyang Times - 8° Semana de 2020",
+        "nome": "The Pyongyang Times - 8° Semana de 2020",
         "link": "https://knowkorea.github.io/PDF/TIMES-2020-8.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Pyongyang Times - 6° Semana de 2020",
+        "nome": "The Pyongyang Times - 6° Semana de 2020",
         "link": "https://knowkorea.github.io/PDF/TIMES-2020-6.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Pyongyang Times - 5° Semana de 2020",
+        "nome": "The Pyongyang Times - 5° Semana de 2020",
         "link": "https://knowkorea.github.io/PDF/TIMES-2020-5.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Pyongyang Times - 4° Semana de 2020",
+        "nome": "The Pyongyang Times - 4° Semana de 2020",
         "link": "https://knowkorea.github.io/PDF/TIMES-2020-4.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Pyongyang Times - 3° Semana de 2020",
+        "nome": "The Pyongyang Times - 3° Semana de 2020",
         "link": "https://knowkorea.github.io/PDF/TIMES-2020-3.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Pyongyang Times - 1° Semana de 2020",
+        "nome": "The Pyongyang Times - 1° Semana de 2020",
         "link": "https://knowkorea.github.io/PDF/TIMES-2020-1.pdf",
         "figura": "bandeira.png",
         "texto": ""
