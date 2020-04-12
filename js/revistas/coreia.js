f70ad2d2191b3131cdee0c425cb75fc2c1fd902f
Thalles Campagnani <thallescampagnani@gmail.com>
2020-04-12 19:08:24 -0300

    Adicionando Links de 2016

--- js/revistas/coreia.js
+++ js/revistas/coreia.js
@@ -1,9 +1,76 @@
 var coreia = [
     'Lista de edições do pictorial mensal: "República Popular Democrática da COREIA".',
+    '2016',
     {
-        "nome": "",
-        "link": "",
-        "figura": "",
+        "nome": "RPD da COREIA - Dezembro de 2016",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2016-12.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Novembro de 2016",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2016-11.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Outubro de 2016",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2016-10.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Setembro de 2016",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2016-9.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Agosto de 2016",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2016-8.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Julho de 2016",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2016-7.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Junho de 2016",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2016-6.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Abril de 2016",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2016-5.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Maio de 2016",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2016-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Março de 2016",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2016-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Fevereiro de 2016",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2016-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "RPD da COREIA - Janeiro de 2016",
+        "link": "https://knowkorea.github.io/PDF/KOREA-2016-1.pdf",
+        "figura": "bandeira.png",
         "texto": ""
     }
 ]
