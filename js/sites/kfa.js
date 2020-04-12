2038c23d5a0812f7d20baf03a8e3a82df8fa7b04
Thalles Campagnani <thallescampagnani@gmail.com>
2020-04-12 20:05:28 -0300

    Traduzindo nomes da KFA

--- js/sites/kfa.js
+++ js/sites/kfa.js
@@ -2,55 +2,49 @@ var kfa =
 [
     'Sites das associações de amizade com a Coreia (KFA)',
     {
-        "nome": "KFA World",
+        "nome": "KFA Mundo",
         "link": "https://korea-dpr.com",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "KFA Brazil",
+        "nome": "KFA Brasil",
         "link": "https://facebook.com/KFA.Brasil",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "KFA USA",
+        "nome": "KFA EUA",
         "link": "https://kfausa.org",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "KFA Euskal Herria",
+        "nome": "KFA País Basco (Espanha)",
         "link": "https://kfa-eh.org",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "KFA Basque Country (Spain)",
-        "link": "https://kfa-eh.org",
-        "figura": "bandeira.png",
-        "texto": ""
-    },
-    {
-        "nome": "KFA Spain",
+        "nome": "KFA Espanha",
         "link": "https://kfaspain.es",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "KFA UK",
+        "nome": "KFA Reino Unido",
         "link": "https://kfauk.com",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "KFA Ireland",
+        "nome": "KFA Irlanda",
         "link": "https://jucheireland.wordpress.com",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "KFA Italy",
+        "nome": "KFA Itália",
         "link": "https://italiacoreapopolare.wordpress.com",
         "figura": "bandeira.png",
         "texto": ""
@@ -62,7 +56,7 @@ var kfa =
         "texto": ""
     },
     {
-        "nome": "KFA Ireland",
+        "nome": "KFA Irlanda",
         "link": "https://jucheireland.wordpress.com",
         "figura": "bandeira.png",
         "texto": ""
@@ -74,13 +68,13 @@ var kfa =
         "texto": ""
     },
     {
-        "nome": "KFA Slovenia",
+        "nome": "KFA Eslovénia",
         "link": "https://kfa-slovenia.blogspot.com",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "KFA Bolivia",
+        "nome": "KFA Bolívia",
         "link": "https://kfa-bolivia.blogspot.com",
         "figura": "bandeira.png",
         "texto": ""
