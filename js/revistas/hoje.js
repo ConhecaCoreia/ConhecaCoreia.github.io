8c6ec114804865f7b38f79f0e5f3354223164798
Thalles Campagnani <thallescampagnani@gmail.com>
2020-04-12 19:21:17 -0300

    Adicionando os anos de 2020 a 2016 da revista coreia hoje

--- js/revistas/hoje.js
+++ js/revistas/hoje.js
@@ -1,9 +1,369 @@
 var hoje = [
     'Lista de edições da revista mensal: "COREIA HOJE".',
+    '2020',
+    /*Descomente quando fazerem o uploud de mais revistas*/
+    /*{
+        "nome": "COREIA HOJE - Dezembro de 2020",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2020-12.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
     {
-        "nome": "",
-        "link": "",
-        "figura": "",
+        "nome": "COREIA HOJE - Novembro de 2020",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2020-11.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Outubro de 2020",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2020-10.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Setembro de 2020",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2020-9.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Agosto de 2020",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2020-8.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Julho de 2020",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2020-7.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Junho de 2020",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2020-6.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Maio de 2020",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2020-5.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },*/
+    {
+        "nome": "COREIA HOJE - Abril de 2020",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2020-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Março de 2020",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2020-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Fevereiro de 2020",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2020-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Janeiro de 2020",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2020-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    '2019',
+    {
+        "nome": "COREIA HOJE - Dezembro de 2019",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2019-12.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Novembro de 2019",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2019-11.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Outubro de 2019",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2019-10.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Setembro de 2019",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2019-9.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Agosto de 2019",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2019-8.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Julho de 2019",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2019-7.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Junho de 2019",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2019-6.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Maio de 2019",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2019-5.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Abril de 2019",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2019-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Março de 2019",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2019-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Fevereiro de 2019",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2019-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Janeiro de 2019",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2019-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    '2018',
+    {
+        "nome": "COREIA HOJE - Dezembro de 2018",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2018-12.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Novembro de 2018",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2018-11.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Outubro de 2018",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2018-10.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Setembro de 2018",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2018-9.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Agosto de 2018",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2018-8.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Julho de 2018",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2018-7.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Junho de 2018",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2018-6.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Maio de 2018",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2018-5.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Abril de 2018",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2018-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Março de 2018",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2018-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Fevereiro de 2018",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2018-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Janeiro de 2018",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2018-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    '2017',
+    {
+        "nome": "COREIA HOJE - Dezembro de 2017",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2017-12.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Novembro de 2017",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2017-11.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Outubro de 2017",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2017-10.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Setembro de 2017",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2017-9.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Agosto de 2017",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2017-8.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Julho de 2017",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2017-7.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Junho de 2017",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2017-6.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Maio de 2017",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2017-5.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Abril de 2017",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2017-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Março de 2017",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2017-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Fevereiro de 2017",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2017-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Janeiro de 2017",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2017-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    '2016',
+    {
+        "nome": "COREIA HOJE - Dezembro de 2016",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2016-12.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Novembro de 2016",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2016-11.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Outubro de 2016",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2016-10.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Setembro de 2016",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2016-9.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Agosto de 2016",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2016-8.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Julho de 2016",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2016-7.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Junho de 2016",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2016-6.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Maio de 2016",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2016-5.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Abril de 2016",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2016-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Março de 2016",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2016-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Fevereiro de 2016",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2016-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "COREIA HOJE - Janeiro de 2016",
+        "link": "https://knowkorea.github.io/PDF/TODAY-2016-1.pdf",
+        "figura": "bandeira.png",
         "texto": ""
     }
 ]
