61ce8976d3bf842508c3eaf96158ee17a8829b85
Thalles Campagnani <thallescampagnani@gmail.com>
2020-04-12 19:24:27 -0300

    Trocando a palavra 'edição' para 'trimestre'

--- js/revistas/comercio.js
+++ js/revistas/comercio.js
@@ -2,103 +2,103 @@ var comercio =
 [
     'Lista de edições da revista: "COMERCIO EXTERIOR DA RPDC".',
     {
-        "nome": "Comercio Exterior - 1° Edição de 2020",
+        "nome": "Comercio Exterior - 1° Trimestre de 2020",
         "link": "https://knowkorea.github.io/PDF/TRADE-2020-1.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 4° Edição de 2019",
+        "nome": "Comercio Exterior - 4° Trimestre de 2019",
         "link": "https://knowkorea.github.io/PDF/TRADE-2019-4.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 3° Edição de 2019",
+        "nome": "Comercio Exterior - 3° Trimestre de 2019",
         "link": "https://knowkorea.github.io/PDF/TRADE-2019-3.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 2° Edição de 2019",
+        "nome": "Comercio Exterior - 2° Trimestre de 2019",
         "link": "https://knowkorea.github.io/PDF/TRADE-2019-2.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 1° Edição de 2019",
+        "nome": "Comercio Exterior - 1° Trimestre de 2019",
         "link": "https://knowkorea.github.io/PDF/TRADE-2019-1.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 4° Edição de 2018",
+        "nome": "Comercio Exterior - 4° Trimestre de 2018",
         "link": "https://knowkorea.github.io/PDF/TRADE-2018-4.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 3° Edição de 2018",
+        "nome": "Comercio Exterior - 3° Trimestre de 2018",
         "link": "https://knowkorea.github.io/PDF/TRADE-2018-3.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 2° Edição de 2018",
+        "nome": "Comercio Exterior - 2° Trimestre de 2018",
         "link": "https://knowkorea.github.io/PDF/TRADE-2018-2.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 1° Edição de 2018",
+        "nome": "Comercio Exterior - 1° Trimestre de 2018",
         "link": "https://knowkorea.github.io/PDF/TRADE-2018-1.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 4° Edição de 2017",
+        "nome": "Comercio Exterior - 4° Trimestre de 2017",
         "link": "https://knowkorea.github.io/PDF/TRADE-2017-4.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 3° Edição de 2017",
+        "nome": "Comercio Exterior - 3° Trimestre de 2017",
         "link": "https://knowkorea.github.io/PDF/TRADE-2017-3.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 2° Edição de 2017",
+        "nome": "Comercio Exterior - 2° Trimestre de 2017",
         "link": "https://knowkorea.github.io/PDF/TRADE-2017-2.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 1° Edição de 2017",
+        "nome": "Comercio Exterior - 1° Trimestre de 2017",
         "link": "https://knowkorea.github.io/PDF/TRADE-2017-1.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 4° Edição de 2016",
+        "nome": "Comercio Exterior - 4° Trimestre de 2016",
         "link": "https://knowkorea.github.io/PDF/TRADE-2016-4.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 3° Edição de 2016",
+        "nome": "Comercio Exterior - 3° Trimestre de 2016",
         "link": "https://knowkorea.github.io/PDF/TRADE-2016-3.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 2° Edição de 2016",
+        "nome": "Comercio Exterior - 2° Trimestre de 2016",
         "link": "https://knowkorea.github.io/PDF/TRADE-2016-2.pdf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
-        "nome": "Comercio Exterior - 1° Edição de 2016",
+        "nome": "Comercio Exterior - 1° Trimestre de 2016",
         "link": "https://knowkorea.github.io/PDF/TRADE-2016-1.pdf",
         "figura": "bandeira.png",
         "texto": ""
