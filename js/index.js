d6d2111eda9afd8490deabb6231681c6af37de1c
Álan Crístoffer <acristoffers@gmail.com>
2020-04-13 13:58:58 -0300

    Implementa URL em SPA

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
-function load_sites(class_name, variavel_sites)
-{ 
+function load_sites(class_name, variavel_sites) {
     sites = document.querySelector(class_name)
     var site_container = null;
     variavel_sites.forEach(e => {
-        if (typeof(e) != 'object')
-        {
-            if (site_container != null){
+        if (typeof (e) != 'object') {
+            if (site_container != null) {
                 sites.appendChild(site_container);
                 site_container = null;
             }
@@ -14,10 +12,8 @@ function load_sites(class_name, variavel_sites)
             category.id = e
             category.appendChild(document.createTextNode(e))
             sites.appendChild(category);
-        }
-        else
-        {
-            if (site_container == null){
+        } else {
+            if (site_container == null) {
                 site_container = document.createElement("div")
                 site_container.className = 'site_container'
             }
@@ -29,16 +25,16 @@ function load_sites(class_name, variavel_sites)
             var figure = document.createElement('a')
             figure.style.height = '100px';
             var img = document.createElement('img')
-            img.src =  'images/' + e['figura']
+            img.src = 'images/' + e['figura']
             figure.appendChild(img)
             figure.className = 'site-' + 'figura'
 
-            var title = document.createElement("div")  
+            var title = document.createElement("div")
             title.appendChild(document.createTextNode(e['nome']))
             title.className = 'site-' + 'nome'
-            
-            var text = document.createElement("div")  
-            text.appendChild(document.createTextNode(e['texto']))        
+
+            var text = document.createElement("div")
+            text.appendChild(document.createTextNode(e['texto']))
             text.className = 'site-' + 'texto'
             var container = document.createElement('div')
             container.className = 'container'
@@ -53,25 +49,48 @@ function load_sites(class_name, variavel_sites)
     sites.appendChild(site_container);
 }
 
-function toggle_page(valor)
-{
-    if (valor == 1)  x = "flex"; else x = "none"; document.querySelector(".home") .style.display = x;
-    if (valor == 2)  x = "flex"; else x = "none"; document.querySelector(".sites").style.display = x;
-    if (valor == 3)  x = "flex"; else x = "none"; document.querySelector(".revistas").style.display = x;
-    if (valor == 4)  x = "flex"; else x = "none"; document.querySelector(".about").style.display = x;
-    if (valor == 5)  x = "flex"; else x = "none"; document.querySelector(".sitesOficial").style.display = x;
-    if (valor == 6)  x = "flex"; else x = "none"; document.querySelector(".sitesKfa").style.display = x;
-    if (valor == 7)  x = "flex"; else x = "none"; document.querySelector(".sitesBlogs").style.display = x;
-    if (valor == 8)  x = "flex"; else x = "none"; document.querySelector(".sitesYoutube").style.display = x;
-    if (valor == 9)  x = "flex"; else x = "none"; document.querySelector(".sitesTurismo").style.display = x;
-    if (valor == 10) x = "flex"; else x = "none"; document.querySelector(".revistasTimes").style.display = x;
-    if (valor == 11) x = "flex"; else x = "none"; document.querySelector(".revistasCoreia").style.display = x;
-    if (valor == 12) x = "flex"; else x = "none"; document.querySelector(".revistasHoje").style.display = x;
-    if (valor == 13) x = "flex"; else x = "none"; document.querySelector(".revistasKumsu").style.display = x;
-    if (valor == 14) x = "flex"; else x = "none"; document.querySelector(".revistasComercio").style.display = x;
+function toggle_page(valor) {
+    const divs = ['home', 'sites', 'revistas', 'about', 'sitesOficial',
+        'sitesKfa', 'sitesBlogs', 'sitesYoutube', 'sitesTurismo',
+        'revistasTimes', 'revistasCoreia', 'revistasHoje',
+        'revistasKumsu', 'revistasComercio'
+    ]
+
+    for (div of divs) {
+        document.querySelector('.' + div).style.display = 'none';
+    }
+
+    document.querySelector('.' + divs[valor - 1]).style.display = 'flex';
+    history.pushState({}, '', '/' + divs[valor - 1]);
 }
 
-toggle_page(1)
+function load_from_url() {
+    const divs = ['home', 'sites', 'revistas', 'about', 'sitesOficial',
+        'sitesKfa', 'sitesBlogs', 'sitesYoutube', 'sitesTurismo',
+        'revistasTimes', 'revistasCoreia', 'revistasHoje',
+        'revistasKumsu', 'revistasComercio'
+    ]
+
+    changeTo = 0;
+    for (div of divs) {
+        if (location.pathname == '/' + div) {
+            changeTo = divs.indexOf(div);
+        }
+    };
+
+    toggle_page(changeTo + 1);
+}
+
+if (location.pathname == '/') {
+    toggle_page(1);
+} else {
+    load_from_url();
+}
+
+window.onpopstate = function (event) {
+    load_from_url();
+}
+
 load_sites(".sitesOficial", oficial)
 load_sites(".sitesKfa", kfa)
 load_sites(".sitesBlogs", blogs)
