46417e44b2c7f436e4011808078990920cbcfa85
Thalles Campagnani <thallescampagnani@gmail.com>
2020-04-14 16:47:21 -0300

    Add TRADE 2006 to 2015

--- js/revistas/comercio.js
+++ js/revistas/comercio.js
@@ -125,6 +125,206 @@ var comercio =
         "link": "https://knowkorea.github.io/PDF/TRADE-2016-1.pdf",
         "figura": "bandeira.png",
         "texto": ""
+    },
+    '2015',
+    {
+        "nome": "Comercio Exterior - 4° Trimestre de 2015",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2015-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },/*
+    {
+        "nome": "Comercio Exterior - 3° Trimestre de 2015",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2015-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },*/
+    {
+        "nome": "Comercio Exterior - 2° Trimestre de 2015",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2015-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "Comercio Exterior - 1° Trimestre de 2015",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2015-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    '2014',
+    {
+        "nome": "Comercio Exterior - 4° Trimestre de 2014",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2014-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "Comercio Exterior - 3° Trimestre de 2014",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2014-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "Comercio Exterior - 2° Trimestre de 2014",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2014-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "Comercio Exterior - 1° Trimestre de 2014",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2014-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    '2013',/*
+    {
+        "nome": "Comercio Exterior - 4° Trimestre de 2013",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2013-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },*/
+    {
+        "nome": "Comercio Exterior - 3° Trimestre de 2013",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2013-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "Comercio Exterior - 2° Trimestre de 2013",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2013-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "Comercio Exterior - 1° Trimestre de 2013",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2013-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    '2012',
+    {
+        "nome": "Comercio Exterior - 4° Trimestre de 2012",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2012-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "Comercio Exterior - 3° Trimestre de 2012",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2012-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },/*
+    {
+        "nome": "Comercio Exterior - 2° Trimestre de 2012",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2012-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },*/
+    {
+        "nome": "Comercio Exterior - 1° Trimestre de 2012",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2012-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    '2010',
+    {
+        "nome": "Comercio Exterior - 4° Trimestre de 2010",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2010-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },/*
+    {
+        "nome": "Comercio Exterior - 3° Trimestre de 2010",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2010-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },*/
+    {
+        "nome": "Comercio Exterior - 2° Trimestre de 2010",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2010-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },/*
+    {
+        "nome": "Comercio Exterior - 1° Trimestre de 2010",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2010-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },*/
+    '2008',/*
+    {
+        "nome": "Comercio Exterior - 4° Trimestre de 2008",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2008-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },*/
+    {
+        "nome": "Comercio Exterior - 3° Trimestre de 2008",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2008-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "Comercio Exterior - 2° Trimestre de 2008",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2008-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },/*
+    {
+        "nome": "Comercio Exterior - 1° Trimestre de 2008",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2008-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },*/
+    '2007',/*
+    {
+        "nome": "Comercio Exterior - 4° Trimestre de 2007",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2007-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "Comercio Exterior - 3° Trimestre de 2007",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2007-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },*/
+    {
+        "nome": "Comercio Exterior - 2° Trimestre de 2007",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2007-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },/*
+    {
+        "nome": "Comercio Exterior - 1° Trimestre de 2007",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2007-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },*/
+    '2006',
+    /*{
+        "nome": "Comercio Exterior - 4° Trimestre de 2006",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2006-4.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "Comercio Exterior - 3° Trimestre de 2006",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2006-3.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "Comercio Exterior - 2° Trimestre de 2006",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2006-2.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
+    },*/
+    {
+        "nome": "Comercio Exterior - 1° Trimestre de 2006",
+        "link": "https://knowkorea.github.io/PDF/TRADE-2006-1.pdf",
+        "figura": "bandeira.png",
+        "texto": ""
     }
 ]
 
