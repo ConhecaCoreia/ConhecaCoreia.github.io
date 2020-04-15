3f4b3b02266aecddbf3f86d058fd369a2371bed0
Thalles Campagnani <thallescampagnani@gmail.com>
2020-04-15 18:06:23 -0300

    Organizando por categoria

--- js/sites/oficial.js
+++ js/sites/oficial.js
@@ -1,29 +1,30 @@
 var oficial =
 [
-    'Sites oficiais da RPD da Coreia',
+    'SITES OFICIAIS DA RÉPUBLICA POPULAR DEMOCRATICA DA COREIA',
     {
         "nome": "Portal RPDC ",
         "link": "http://dprkportal.kp",
         "figura": "bandeira.png",
-        "texto": "Portal Oficial de sites da RPD-Coreia"
+        "texto": "Portal oficial de sites da RPD-Coreia"
     },
     {
-        "nome": "Livros Coreanos",
-        "link": "http://korean-books.com.kp/en",
+        "nome": "Naenara",
+        "link": "http://naenara.com.kp",
         "figura": "bandeira.png",
-        "texto": "Site com livros da RPD-Coreia, além de artigos, revistas, pictoriais, etc. (em varios idiomas)."
+        "texto": "É o portal oficial do Governo da RPD-Coreia."
+    },
+    'Jornais',/***********************************************/
+    {
+        "nome": "Agencia Central de Noticias Coreanas",
+        "link": "http://kcna.kp",
+        "figura": "bandeira.png",
+        "texto": "Site da agencia de noticias da RPD-Coreia."
     },
     {
         "nome": "Media Ryugyong",
         "link": "http://mediaryugyong.com.kp",
         "figura": "bandeira.png",
         "texto": "Site com fotos e vídeos sobre a RPD-Coreia."
-    },
-    {
-        "nome": "Agencia Central de Noticias Coreanas",
-        "link": "http://kcna.kp",
-        "figura": "bandeira.png",
-        "texto": "Site da agencia de noticias da RPD-Coreia."
     },
     {
         "nome": "Jornal 'The Pyongyang Times'",
@@ -38,22 +39,23 @@ var oficial =
         "texto": "É a versão online do Jornal dos Trabalhadores (Rodong Sinmun), o jornal oficial do Partido dos Trabalhadores da Coreia."
     },
     {
-        "nome": "Naenara",
-        "link": "http://naenara.com.kp",
-        "figura": "bandeira.png",
-        "texto": "É o portal oficial do Governo da RPD-Coreia."
-    },
-    {
         "nome": "Voz da Coreia",
         "link": "http://vok.rep.kp",
         "figura": "bandeira.png",
         "texto": "Site da rede de radio 'Voz da Coreia'."
     },
+    'Educação & Ciência',/******************************************/
     {
-        "nome": "Grande Unidade Nacional - GNU",
-        "link": "http://gnu.rep.kp",
+        "nome": "Conselho de Educação da RPD Coréia",
+        "link": "http://ryomyong.edu.kp",
         "figura": "bandeira.png",
-        "texto": "Site em coreano destinado ao público na Coréia do Sul, Japão e China com foco na unificação da península coreana."
+        "texto": ""
+    },
+    {
+        "nome": "Livros Coreanos",
+        "link": "http://korean-books.com.kp/en",
+        "figura": "bandeira.png",
+        "texto": "Site com livros da RPD-Coreia, além de artigos, revistas, pictoriais, etc. (em varios idiomas)."
     },
     {
         "nome": "Universidade Kim Il Sung",
@@ -68,6 +70,19 @@ var oficial =
         "texto": "Site da Universidade de Tecnologia Kim Chaek."
     },
     {
+        "nome": "Associação Coreana de Cientistas Sociais",
+        "link": "http://kass.org.kp",
+        "figura": "bandeira.png",
+        "texto": "A Associação é uma organização acadêmica não permanente nacional, que inclui acadêmicos de instituições de ensino e pesquisa de ciências sociais da Coreia do Norte."
+    },
+    {
+        "nome": "Comissão de Estado de Ciência e Tecnologia",
+        "link": "http://mirae.aca.kp",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    'Relações Exteriores',/**************************************** */
+    {
         "nome": "Ministério das Relações Exteriores",
         "link": "http://mfa.gov.kp/en",
         "figura": "bandeira.png",
@@ -80,17 +95,12 @@ var oficial =
         "texto": "Site com notícias, e-books e informações sobre intercâmbio cultural."
     },
     {
-        "nome": "Associação Coreana de Cientistas Sociais",
-        "link": "http://kass.org.kp",
+        "nome": "Compania Internacional de Viagens de Jovens e Crianças para Coreia",
+        "link": "http://kiyctc.com.kp",
         "figura": "bandeira.png",
-        "texto": "A Associação é uma organização acadêmica não permanente nacional, que inclui acadêmicos de instituições de ensino e pesquisa de ciências sociais da Coreia do Norte."
+        "texto": "O KIYCTC ocupa o topo do ranking de empresas de viagens internacionais na RPDC. \n A importante missão da empresa é organizar o turismo na RPDC em estreito contato com empresas de viagens de outros países. Também organiza o intercâmbio e acampamento de jovens e crianças de países estrangeiros como o Escritório Internacional de Intercâmbio de Jovens e Crianças da Coréia (KIYCEB)."
     },
-    {
-        "nome": "Liga da Juventude Juche",
-        "link": "http://youth.rep.kp",
-        "figura": "bandeira.png",
-        "texto": "Site da Juventude do Partido dos Trabalhadores da Coreia. A principal organização juvenil da Coréia do Norte."
-    },
+    'Turismo',/******************************************************* */
     {
         "nome": "Air Koryo",
         "link": "http://airkoryo.com.kp",
@@ -98,17 +108,37 @@ var oficial =
         "texto": "Site da empresa estatal de viagens aereas."
     },
     {
+        "nome": "Administração Nacional de Turismo",
+        "link": "http://tourismdprk.gov.kp",
+        "figura": "bandeira.png",
+        "texto": "É uma organização estatal encarregada da orientação e administração unificadas de assuntos turísticos no país."
+    },
+    'Cultura e Esportes',/******************************************************** */
+    {
+        "nome": "Ministério da Cultura da RPDC",
+        "link": "http://korart.sca.kp",
+        "figura": "bandeira.png",
+        "texto": "Este site representa o Ministério da Cultura da RPDC na internet"
+    },
+    {
+        "nome": "KorFilms",
+        "link": "http://korfilm.com.kp",
+        "figura": "bandeira.png",
+        "texto": "Site do Festival Internacional de Cinema de Pyongang"
+    },
+    {
+        "nome": "Esportes",
+        "link": "http://sdprk.org.kp",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
         "nome": "Cozinheiros",
         "link": "http://cooks.org.kp",
         "figura": "bandeira.png",
         "texto": "Site da Associação Coreana de Cozinheiros. Inclui receitas e críticas de restaurantes."
     },
-    {
-        "nome": "Fundo de Educação da Coréia",
-        "link": "http://koredufund.org.kp",
-        "figura": "bandeira.png",
-        "texto": "Site em inglês e coreano de uma organização não governamental que coordena o apoio à educação"
-    },
+    'Comercio e Empresas',/**************************************************/
     {
         "nome": "Manmulsang",
         "link": "http://manmulsang.com.kp",
@@ -122,106 +152,10 @@ var oficial =
         "texto": "Organização envolvida de forma independente no trabalho de inteligência financeira para conduzir AML e CFT. Garanta a segurança do sistema financeiro. Impedir atividades de ML e FT. Cooperação internacional e intercâmbio com as organizações nacionais e internacionais relevantes. Recebimento, coleta, análise, disseminação de transações suspeitas, inclusive em grandes somas, relevantes para ML e FT."
     },
     {
-        "nome": "KorFilms",
-        "link": "http://korfilm.com.kp",
-        "figura": "bandeira.png",
-        "texto": "Site do Festival Internacional de Cinema de Pyongang"
-    },
-    {
-        "nome": "Administração Nacional de Turismo",
-        "link": "http://tourismdprk.gov.kp",
-        "figura": "bandeira.png",
-        "texto": "É uma organização estatal encarregada da orientação e administração unificadas de assuntos turísticos no país."
-    },
-    {
-        "nome": "Esportes",
-        "link": "http://sdprk.org.kp",
-        "figura": "bandeira.png",
-        "texto": ""
-    },
-    {
-        "nome": "minzu.rep.kp",
-        "link": "http://minzu.rep.kp",
-        "figura": "bandeira.png",
-        "texto": ""
-    },
-    {
-        "nome": "klic.com.kp",
-        "link": "http://klic.com.kp",
-        "figura": "bandeira.png",
-        "texto": ""
-    },
-    {
         "nome": "Comercio Exterior da RPD-Coreia",
         "link": "http://kftrade.com.kp ",
         "figura": "bandeira.png",
         "texto": ""
-    },
-    {
-        "nome": "Compania Internacional de Viagens de Jovens e Crianças para Coreia",
-        "link": "http://kiyctc.com.kp",
-        "figura": "bandeira.png",
-        "texto": "O KIYCTC ocupa o topo do ranking de empresas de viagens internacionais na RPDC. \n A importante missão da empresa é organizar o turismo na RPDC em estreito contato com empresas de viagens de outros países. Também organiza o intercâmbio e acampamento de jovens e crianças de países estrangeiros como o Escritório Internacional de Intercâmbio de Jovens e Crianças da Coréia (KIYCEB)."
-    },
-    {
-        "nome": "pulbora.edu.kp",
-        "link": "http://pulbora.edu.kp",
-        "figura": "bandeira.png",
-        "texto": ""
-    },
-    {
-        "nome": "gpsh.edu.kp",
-        "link": "http://gpsh.edu.kp",
-        "figura": "bandeira.png",
-        "texto": ""
-    },
-    {
-        "nome": "Fundo de Assistência ao Idoso da Coreia",
-        "link": "http://korelcfund.org.kp",
-        "figura": "bandeira.png",
-        "texto": "A Federação de Proteção Joseon Roja está comprometida em garantir os direitos e interesses dos idosos na República Popular Democrática da Coréia e em torná-los mais gratificantes e felizes, mental e fisicamente saudáveis.  Para planejar, negociar e implementar o projeto unificado de proteção a idosos, que é uma das políticas sociais importantes do país, o comitê de proteção a idosos é organizado e operado no gabinete, províncias, cidades e condados. Para esse fim, em 30 de abril de Juche 92 (2003), foi fundada a Federação de Proteção Joseon Roja."
-    },
-    {
-        "nome": "Comissão de Estado de Ciência e Tecnologia",
-        "link": "http://mirae.aca.kp",
-        "figura": "bandeira.png",
-        "texto": ""
-    },
-    {
-        "nome": "Administração Maritima da RPD Coreia",
-        "link": "http://ma.gov.kp",
-        "figura": "bandeira.png",
-        "texto": ""
-    },
-    {
-        "nome": "Conselho de Educação da RPD Coréia",
-        "link": "http://ryomyong.edu.kp",
-        "figura": "bandeira.png",
-        "texto": ""
-    },
-    {
-        "nome": "Ministério da Cultura da RPDC",
-        "link": "http://korart.sca.kp",
-        "figura": "bandeira.png",
-        "texto": "Este site representa o Ministério da Cultura da RPDC na internet"
-    },
-    {
-        "nome": "Fundação Coreia Verde",
-        "link": "http://naenara.com.kp/sites/kgf",
-        "figura": "bandeira.png",
-        "texto": "É a ONG que procura ajudar o povo coreano a manter sua terra de tapeçaria dourada de 3000 ri em melhores condições e melhorá-la maravilhosamente no presente momento . Também desenvolve esforços conjuntos da comunidade internacional para alcançar o objetivo comum de alcançar um futuro próspero e seguro em prol da humanidade e da Terra. A missão do KGF é oferecer apoio financeiro, material e técnico para a promoção do desenvolvimento verde, a fim de alcançar progresso econômico sustentado, ambiente ecológico limpo e condições de vida civilizadas e outras possibilidades relevantes."
-    },
-    {
-        "nome": "Fundação Kim Il Sung- Kim Jong Il",
-        "link": "http://naenara.com.kp/sites/kkf",
-        "figura": "bandeira.png",
-        "texto": "A missão da Fundação Kim Il Sung - Kim Jong Il é preservar o Palácio Kumsusan do Sol, onde estão os grandes líderes do estado, como o santuário supremo eterno dos Juche e arrecadar fundos para educação, saúde pública e proteção ambiental. , bem como pela paz e progresso da humanidade, de acordo com as instruções dos grandes líderes que dedicaram tudo ao bem-estar do povo."
-    },
-    {
-        "nome": "Federação Coreana para a Proteção das Pessoas com Deficiência",
-        "link": "http://naenara.com.kp/sites/kfpd",
-        "figura": "bandeira.png",
-        "texto": "O KFPD é a única organização não governamental relacionada às pessoas com deficiência aprovada pelo governo e inclui todas as organizações envolvidas nos assuntos das pessoas com deficiência. - Missão e Tarefa A missão da KFPD é representar e defender os direitos e interesses de todas as pessoas com deficiência na Coreia do Norte."
     },
     {
         "nome": "Coreia Pyongyang Ryugyong Alimentos",
@@ -230,18 +164,49 @@ var oficial =
         "texto": "Site da Compania Coreia Pyongyang Ryugyong Alimentos. Seu principal negócio é o serviço de cozinha tradicional coreana, a produção de alimentos tradicionais coreanos, incluindo kimchi, a comercialização de pratos embalados, bebidas e artesanato e a troca de pratos nacionais com vários países."
     },
     {
+        "nome": "Parque de Desenvolvimento de Alta Teconologia de Unjong",
+        "link": "http://naenara.com.kp/sites/unjong/",
+        "figura": "bandeira.png",
+        "texto": "O Parque de Desenvolvimento de Alta Tecnologia Unjong é um dos parques de desenvolvimento econômico a serem criados de acordo com o plano do governo de fazer um avanço no esforço de construir uma economia baseada no conhecimento."
+    },
+    "Fundações, Federações e ONG's",/********************************** */
+    {
+        "nome": "Fundo de Educação da Coréia",
+        "link": "http://koredufund.org.kp",
+        "figura": "bandeira.png",
+        "texto": "Site em inglês e coreano de uma organização não governamental que coordena o apoio à educação"
+    },
+    {
         "nome": "Fundo Nacional de Proteção ao Patrimônio da Coreia",
         "link": "http://naenara.com.kp/sites/national/",
         "figura": "bandeira.png",
         "texto": "A missão do Fundo é receber contribuições, incluindo dados históricos valiosos, relíquias, materiais e fundos de todo o povo coreano do norte, sul e exterior e de povos progressistas do mundo, a fim de enriquecer o tesouro do patrimônio nacional e contribuir à conservação do patrimônio nacional, como escavação, verificação, preservação e gerenciamento do patrimônio."
     },
     {
-        "nome": "Parque de Desenvolvimento de Alta Teconologia de Unjong",
-        "link": "http://naenara.com.kp/sites/unjong/",
+        "nome": "Federação Coreana para a Proteção das Pessoas com Deficiência",
+        "link": "http://naenara.com.kp/sites/kfpd",
         "figura": "bandeira.png",
-        "texto": "O Parque de Desenvolvimento de Alta Tecnologia Unjong é um dos parques de desenvolvimento econômico a serem criados de acordo com o plano do governo de fazer um avanço no esforço de construir uma economia baseada no conhecimento."
+        "texto": "O KFPD é a única organização não governamental relacionada às pessoas com deficiência aprovada pelo governo e inclui todas as organizações envolvidas nos assuntos das pessoas com deficiência. - Missão e Tarefa A missão da KFPD é representar e defender os direitos e interesses de todas as pessoas com deficiência na Coreia do Norte."
     },
-    'Compania de Seguros',
+    {
+        "nome": "Fundação Kim Il Sung- Kim Jong Il",
+        "link": "http://naenara.com.kp/sites/kkf",
+        "figura": "bandeira.png",
+        "texto": "A missão da Fundação Kim Il Sung - Kim Jong Il é preservar o Palácio Kumsusan do Sol, onde estão os grandes líderes do estado, como o santuário supremo eterno dos Juche e arrecadar fundos para educação, saúde pública e proteção ambiental. , bem como pela paz e progresso da humanidade, de acordo com as instruções dos grandes líderes que dedicaram tudo ao bem-estar do povo."
+    },
+    {
+        "nome": "Fundação Coreia Verde",
+        "link": "http://naenara.com.kp/sites/kgf",
+        "figura": "bandeira.png",
+        "texto": "É a ONG que procura ajudar o povo coreano a manter sua terra de tapeçaria dourada de 3000 ri em melhores condições e melhorá-la maravilhosamente no presente momento . Também desenvolve esforços conjuntos da comunidade internacional para alcançar o objetivo comum de alcançar um futuro próspero e seguro em prol da humanidade e da Terra. A missão do KGF é oferecer apoio financeiro, material e técnico para a promoção do desenvolvimento verde, a fim de alcançar progresso econômico sustentado, ambiente ecológico limpo e condições de vida civilizadas e outras possibilidades relevantes."
+    },
+    {
+        "nome": "Fundo de Assistência ao Idoso da Coreia",
+        "link": "http://korelcfund.org.kp",
+        "figura": "bandeira.png",
+        "texto": "A Federação de Proteção Joseon Roja está comprometida em garantir os direitos e interesses dos idosos na República Popular Democrática da Coréia e em torná-los mais gratificantes e felizes, mental e fisicamente saudáveis.  Para planejar, negociar e implementar o projeto unificado de proteção a idosos, que é uma das políticas sociais importantes do país, o comitê de proteção a idosos é organizado e operado no gabinete, províncias, cidades e condados. Para esse fim, em 30 de abril de Juche 92 (2003), foi fundada a Federação de Proteção Joseon Roja."
+    },
+    'Compania de Seguros',/***************************************** */
     {
         "nome": "Corporação Nacional de Seguros da Coreia",
         "link": "http://knic.com.kp",
@@ -271,5 +236,48 @@ var oficial =
         "link": "http://naenara.com.kp/sites/polestar/",/*http://www.polestar.com.kp/*/
         "figura": "bandeira.png",
         "texto": "A Polestar foi lançada como uma companhia de seguros imobiliários especializada em um cenário de crescimento dinâmico na economia nacional, com impulso para a frente reunido recentemente e aumento subsequente na necessidade da comunidade de uma cobertura mais ampla. Hoje nossa cobertura inclui incêndio, engenharia, agricultura e crédito."
+    },
+    'Outros',/****************** */
+    {
+        "nome": "Administração Maritima da RPD Coreia",
+        "link": "http://ma.gov.kp",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "Grande Unidade Nacional - GNU",
+        "link": "http://gnu.rep.kp",
+        "figura": "bandeira.png",
+        "texto": "Site em coreano destinado ao público na Coréia do Sul, Japão e China com foco na unificação da península coreana."
+    },
+    {
+        "nome": "Liga da Juventude Juche",
+        "link": "http://youth.rep.kp",
+        "figura": "bandeira.png",
+        "texto": "Site da Juventude do Partido dos Trabalhadores da Coreia. A principal organização juvenil da Coréia do Norte."
+    },
+    {
+        "nome": "minzu.rep.kp",
+        "link": "http://minzu.rep.kp",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "klic.com.kp",
+        "link": "http://klic.com.kp",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "pulbora.edu.kp",
+        "link": "http://pulbora.edu.kp",
+        "figura": "bandeira.png",
+        "texto": ""
+    },
+    {
+        "nome": "gpsh.edu.kp",
+        "link": "http://gpsh.edu.kp",
+        "figura": "bandeira.png",
+        "texto": ""
     }
 ]
