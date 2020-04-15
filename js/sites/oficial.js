f62d6f3c8e10a8b681fc4309965972d29a0686e7
Thalles Campagnani <thallescampagnani@gmail.com>
2020-04-15 14:40:26 -0300

    Change HTTPS to HTTP

--- js/sites/oficial.js
+++ js/sites/oficial.js
@@ -3,235 +3,235 @@ var oficial =
     'Sites oficiais da RPD da Coreia',
     {
         "nome": "Portal RPDC ",
-        "link": "https://dprkportal.kp",
+        "link": "http://dprkportal.kp",
         "figura": "bandeira.png",
         "texto": "Portal Oficial de sites da RPD-Coreia"
     },
     {
         "nome": "Livros Coreanos",
-        "link": "https://korean-books.com.kp/en",
+        "link": "http://korean-books.com.kp/en",
         "figura": "bandeira.png",
         "texto": "Site com livros da RPD-Coreia, além de artigos, revistas, pictoriais, etc. (em varios idiomas)."
     },
     {
         "nome": "Media Ryugyong",
-        "link": "https://mediaryugyong.com.kp",
+        "link": "http://mediaryugyong.com.kp",
         "figura": "bandeira.png",
         "texto": "Site com fotos e vídeos sobre a RPD-Coreia."
     },
     {
         "nome": "Agencia Central de Noticias Coreanas",
-        "link": "https://kcna.kp",
+        "link": "http://kcna.kp",
         "figura": "bandeira.png",
         "texto": "Site da agencia de noticias da RPD-Coreia."
     },
     {
         "nome": "Jornal 'The Pyongyang Times'",
-        "link": "https://pyongyangtimes.com.kp",
+        "link": "http://pyongyangtimes.com.kp",
         "figura": "bandeira.png",
         "texto": "Versão online do jornal 'The Pyongyang Times', a versão em língua estrangeira do 'Pyongyang Sinmun', que é um jornal da capital da RPD-Coreia."
     },
     {
         "nome": "Jornal dos Trabalhadores",
-        "link": "https://rodong.rep.kp",
+        "link": "http://rodong.rep.kp",
         "figura": "bandeira.png",
         "texto": "É a versão online do Jornal dos Trabalhadores (Rodong Sinmun), o jornal oficial do Partido dos Trabalhadores da Coreia."
     },
     {
         "nome": "Naenara",
-        "link": "https://naenara.com.kp",
+        "link": "http://naenara.com.kp",
         "figura": "bandeira.png",
         "texto": "É o portal oficial do Governo da RPD-Coreia."
     },
     {
         "nome": "Voz da Coreia",
-        "link": "https://vok.rep.kp",
+        "link": "http://vok.rep.kp",
         "figura": "bandeira.png",
         "texto": "Site da rede de radio 'Voz da Coreia'."
     },
     {
         "nome": "Grande Unidade Nacional - GNU",
-        "link": "https://gnu.rep.kp",
+        "link": "http://gnu.rep.kp",
         "figura": "bandeira.png",
         "texto": "Site em coreano destinado ao público na Coréia do Sul, Japão e China com foco na unificação da península coreana."
     },
     {
         "nome": "Universidade Kim Il Sung",
-        "link": "https://ryongnamsan.edu.kp",
+        "link": "http://ryongnamsan.edu.kp",
         "figura": "bandeira.png",
         "texto": "Site da Universidade Kim Il Sung."
     },
     {
         "nome": "Universidade de Tecnologia Kim Chaek",
-        "link": "https://kut.edu.kp",
+        "link": "http://kut.edu.kp",
         "figura": "bandeira.png",
         "texto": "Site da Universidade de Tecnologia Kim Chaek."
     },
     {
         "nome": "Ministério das Relações Exteriores",
-        "link": "https://mfa.gov.kp/en",
+        "link": "http://mfa.gov.kp/en",
         "figura": "bandeira.png",
         "texto": "Site do Ministério das Relações Exteriores."
     },
     {
         "nome": "Comitê de Relações Culturais com Países Estrangeiros",
-        "link": "https://friend.com.kp",
+        "link": "http://friend.com.kp",
         "figura": "bandeira.png",
         "texto": "Site com notícias, e-books e informações sobre intercâmbio cultural."
     },
     {
         "nome": "Associação Coreana de Cientistas Sociais",
-        "link": "https://kass.org.kp",
+        "link": "http://kass.org.kp",
         "figura": "bandeira.png",
         "texto": "A Associação é uma organização acadêmica não permanente nacional, que inclui acadêmicos de instituições de ensino e pesquisa de ciências sociais da Coreia do Norte."
     },
     {
         "nome": "Liga da Juventude Juche",
-        "link": "https://youth.rep.kp",
+        "link": "http://youth.rep.kp",
         "figura": "bandeira.png",
         "texto": "Site da Juventude do Partido dos Trabalhadores da Coreia. A principal organização juvenil da Coréia do Norte."
     },
     {
         "nome": "Air Koryo",
-        "link": "https://airkoryo.com.kp",
+        "link": "http://airkoryo.com.kp",
         "figura": "bandeira.png",
         "texto": "Site da empresa estatal de viagens aereas."
     },
     {
         "nome": "Cozinheiros",
-        "link": "https://cooks.org.kp",
+        "link": "http://cooks.org.kp",
         "figura": "bandeira.png",
         "texto": "Site da Associação Coreana de Cozinheiros. Inclui receitas e críticas de restaurantes."
     },
     {
         "nome": "Fundo de Educação da Coréia",
-        "link": "https://koredufund.org.kp",
+        "link": "http://koredufund.org.kp",
         "figura": "bandeira.png",
         "texto": "Site em inglês e coreano de uma organização não governamental que coordena o apoio à educação"
     },
     {
         "nome": "Manmulsang",
-        "link": "https://manmulsang.com.kp",
+        "link": "http://manmulsang.com.kp",
         "figura": "bandeira.png",
         "texto": "É um estabelecimento que se dedica exclusivamente ao comércio eletrônico, comércio exterior e diferentes tipos de indústria de serviços na RPD-Coreia."
     },
     {
         "nome": "Agência de Inteligência Financeira",
-        "link": "https://fia.law.kp",
+        "link": "http://fia.law.kp",
         "figura": "bandeira.png",
         "texto": "Organização envolvida de forma independente no trabalho de inteligência financeira para conduzir AML e CFT. Garanta a segurança do sistema financeiro. Impedir atividades de ML e FT. Cooperação internacional e intercâmbio com as organizações nacionais e internacionais relevantes. Recebimento, coleta, análise, disseminação de transações suspeitas, inclusive em grandes somas, relevantes para ML e FT."
     },
     {
         "nome": "KorFilms",
-        "link": "https://korfilm.com.kp",
+        "link": "http://korfilm.com.kp",
         "figura": "bandeira.png",
         "texto": "Site do Festival Internacional de Cinema de Pyongang"
     },
     {
         "nome": "Administração Nacional de Turismo",
-        "link": "https://tourismdprk.gov.kp",
+        "link": "http://tourismdprk.gov.kp",
         "figura": "bandeira.png",
         "texto": "É uma organização estatal encarregada da orientação e administração unificadas de assuntos turísticos no país."
     },
     {
         "nome": "Esportes",
-        "link": "https://sdprk.org.kp",
+        "link": "http://sdprk.org.kp",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "minzu.rep.kp",
-        "link": "https://minzu.rep.kp",
+        "link": "http://minzu.rep.kp",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "klic.com.kp",
-        "link": "https://klic.com.kp",
+        "link": "http://klic.com.kp",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "kftrade.com.kp ",
-        "link": "https://kftrade.com.kp ",
+        "link": "http://kftrade.com.kp ",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "kiyctc.com.kp",
-        "link": "https://kiyctc.com.kp",
+        "link": "http://kiyctc.com.kp",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "pulbora.edu.kp",
-        "link": "https://pulbora.edu.kp",
+        "link": "http://pulbora.edu.kp",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "gpsh.edu.kp",
-        "link": "https://gpsh.edu.kp",
+        "link": "http://gpsh.edu.kp",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "korelcfund.org.kp",
-        "link": "https://korelcfund.org.kp",
+        "link": "http://korelcfund.org.kp",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "mirae.aca.kp",
-        "link": "https://mirae.aca.kp",
+        "link": "http://mirae.aca.kp",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "knic.com.kp",
-        "link": "https://knic.com.kp",
+        "link": "http://knic.com.kp",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "ma.gov.kp",
-        "link": "https://ma.gov.kp",
+        "link": "http://ma.gov.kp",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "ryomyong.edu.kp",
-        "link": "https://ryomyong.edu.kp",
+        "link": "http://ryomyong.edu.kp",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "korart.sca.kp",
-        "link": "https://korart.sca.kp",
+        "link": "http://korart.sca.kp",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "Kgf",
-        "link": "https://naenara.com.kp/sites/kgf",
+        "link": "http://naenara.com.kp/sites/kgf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "Kkf",
-        "link": "https://naenara.com.kp/sites/kkf",
+        "link": "http://naenara.com.kp/sites/kkf",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "Kfpd",
-        "link": "https://naenara.com.kp/sites/kfpd",
+        "link": "http://naenara.com.kp/sites/kfpd",
         "figura": "bandeira.png",
         "texto": ""
     },
     {
         "nome": "Ryugyong",
-        "link": "https://naenara.com.kp/sites/ryugyong",
+        "link": "http://naenara.com.kp/sites/ryugyong",
         "figura": "bandeira.png",
         "texto": ""
     },
@@ -261,7 +261,7 @@ var oficial =
     },
     {
         "nome": "Rainbow",
-        "link": "https://naenara.com.kp/sites/rainbow",
+        "link": "http://naenara.com.kp/sites/rainbow",
         "figura": "bandeira.png",
         "texto": ""
     },
