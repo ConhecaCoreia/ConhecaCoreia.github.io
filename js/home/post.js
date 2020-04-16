a120ccf4b4c9a534d8a2f1f16cc39fd966acba50
Thalles Campagnani <thallescampagnani@gmail.com>
2020-04-16 12:51:29 -0300

    Separando posts do codigo de trocar de post

--- js/home/post.js
+++ js/home/post.js
@@ -1,22 +1,20 @@
 function nextpost() {
-    if (N2 === post.length - 1){
+    if (N2 === posts.length - 1){
         N2 = 0
     }else{
         N2++;
     }
-    document.getElementsByClassName("home-post")[0].src = post[N2];
+    document.getElementsByClassName("home-post")[0].src = posts[N2];
 }
 
 function previouspost() {
     if (N2 < 1){
-        N2 = post.length - 1
+        N2 = posts.length - 1
     }else{
         N2--;
     }
-    document.getElementsByClassName("home-post")[0].src = post[N2];
+    document.getElementsByClassName("home-post")[0].src = posts[N2];
 }
 
 
 var N2 = 0;
-
-var post = []
---
+++ js/home/posts.js
@@ -0,0 +1,18 @@
+var posts =
+[
+    {
+        "nome": "Questão LGBT na RPD da Coreia",
+        "link": "https://facebook.com/",
+        "texto": "Teste"
+    },
+    {
+        "nome": "Direitos da mulher na RPD da Coreia",
+        "link": "https://facebook.com/",
+        "texto": "Teste2"
+    },
+    {
+        "nome": "Eleições na RPD da Coreia",
+        "link": "https://facebook.com/",
+        "texto": "Teste3"
+    }
+]
