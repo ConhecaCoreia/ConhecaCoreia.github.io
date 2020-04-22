b3ec2a93a3693c8a80ac605b05106f0f6719bb90
bentoavb <bentoavb@gmail.com>
2020-04-22 20:16:37 -0300

    Atualizando imagens da página principal

--- js/home/image.js
+++ js/home/image.js
@@ -1,26 +1,63 @@
+function loadImage(){
+    document.getElementsByClassName("home-image-href")[0].href = images[Nfolder][N];
+    document.getElementsByClassName("home-image-src")[0].src = images[Nfolder][N];
+}
+
 function nextimage() {
-    if (N === images.length - 1){
-        N = 0
+    if (N === images[Nfolder].length - 1){
+        nextfolder()
     }else{
         N++;
     }
-    document.getElementsByClassName("home-image-href")[0].href = images[N];
-    document.getElementsByClassName("home-image-src")[0].src = images[N];
+    loadImage()
 }
 
 function previousimage() {
     if (N < 1){
-        N = images.length - 1
+        previousfolder()
     }else{
         N--;
     }
-    document.getElementsByClassName("home-image-href")[0].href = images[N];
-    document.getElementsByClassName("home-image-src")[0].src = images[N];
+   loadImage()
+}
+
+function nextfolder() {
+    if (Nfolder === images.length - 1){
+        Nfolder = 0
+    }else{
+        Nfolder++;
+    }
+    N = 0
+    loadImage()
+}
+
+function previousfolder() {
+    if (Nfolder < 1){
+        Nfolder = images.length - 1
+    }else{
+        Nfolder--;
+    }
+    N = 0
+    loadImage()
 }
 
 var N = 0;
+var Nfolder = 0;
 
-var images = []
-for (let i = 0; i <= 10; i++) {
-    images.push(`./../images/home/0/${i}.jpg`)
+
+var images = [[],[],[]]
+var qntd   = [10,15,11]
+
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j <= qntd[i]; j++) {
+        if (i == 2 && (j == 2 || j == 8 || j == 10) )
+        {
+            images[i].push(`./../images/home/${i}/${j}.png`)
+        }
+        else {
+            images[i].push(`./../images/home/${i}/${j}.jpg`)
+        }
+        
+    }
 }
+
