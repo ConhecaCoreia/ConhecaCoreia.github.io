80f932b166167e4bc43dd717583487b329679c46
Thalles Campagnani <thallescampagnani@gmail.com>
2020-04-23 18:29:43 -0300

    Add post and images "corona"

---
+++ js/home/posts/corona.js
@@ -0,0 +1,30 @@
+var corona = {
+    "nome": "corona",
+    "titulo": "Porque a Coreia Popular não tem NENHUM caso de COVID-19?",
+    "link": "https://www.facebook.com/KFA.Brasil/posts/2868542033229300",
+    "qtdImg": 60,
+    "texto": `
+    A Coreia Popular não tem nenhum caso de corona virus, mesmo fazendo fronteira com a China, por causa das rapidas medidas que o Estado e seu Sistema Público de Saúde tomaram com o objetivo de fazer a profilaxia e prevenção contra a pandemia.
+    <br>
+    <br>Estas foram as medidas tomadas já no mês de Janeiro e Fevereiro:
+    <br>
+    <br>-Foi criado o Departamento Emergencial Anti-Epidemia.
+    <br>-Foram fechadas as fronteiras com a China (dia 22 de Janeiro).
+    <br>-Os viajantes, turistas e suspeitos foram colocados em quarentena de 15 dias.
+    <br>-Foram isoladas as províncias que fazem fronteira com a China.
+    <br>-Iniciou uma campanha de informação e conscientização na TV, radio, internet e até carros de som.
+    <br>-Distribuiu mascaras GRATUITAMENTE para população e solicitou o uso de todos os cidadãos.
+    <br>-Solicitou o uso de álcool em gel sempre que tocar em uma superfície num espaço público.
+    <br>-Foi iniciada a higienização recorrente de todos espaços públicos, ruas, ônibus, metro, restaurantes, calçadas, trens, etc.
+    <br>-Começou a testar a população nos espaços publicos para sintomas da nova doença, como febre por exemplo.
+    <br>-Reverteu a produção industrial para produzir EPI's, químicos desinfetantes, álcool em gel, mascaras, etc.
+    <br>-Etc.
+    <br>
+    <br>Educação profilática contra o coronavírus na televisão coreana: <a href="https://youtu.be/s0V4I3TpVic">youtu.be/s0V4I3TpVic</a> 
+    <br>
+    <br>Fonte da quantidade de casos: <a href="https://www.reuters.com/article/us-health-coronavirus-northkorea/north-korea-testing-quarantining-for-covid-19-still-says-no-cases-who-representative-idUSKBN21P3C2">reuters.com/article/us-health-coronavirus-northkorea</a>
+    <br>    
+    <br>
+    <br>Adaptado de: KFA - Brasil - Associação de Amizade com a Coreia
+    `
+}
